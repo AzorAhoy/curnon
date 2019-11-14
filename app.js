2ec540ba3b6b039143d312568ff604fc110c7f36
quanganh.tran <quanganh.tran@192.168.1.51>
2019-11-14 22:21:00 +0700

    test

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ var path = require('path');
 //     next(); // cho chuong trinh tiep tuc di
 // });
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
-app.options("*", cors());
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// });
+// app.options("*", cors());
 
 /*Adds the react production build to serve react requests*/
 app.use(express.static(path.join(__dirname, "/public")));
